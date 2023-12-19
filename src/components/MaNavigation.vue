ade641ecb1c77a011c5a9ca75c81deb8f9e1e426
Lucas <desaintem.lucas@gmail.com>
2023-12-19 13:31:53 +0100

    logo dashboard sidebar

--- src/components/MaNavigation.vue
+++ src/components/MaNavigation.vue
@@ -4,7 +4,7 @@
     <div class="flex min-h-screen">
 
       <nav class="flex w-full bg-gray-200 h-28">
-        Lucas DSM
+        
       </nav>
       <!-- sidebar -->
       <div class="sidebar bg-gray-200 h-screen fixed w-[300px] p-4">
@@ -13,51 +13,46 @@
           <img src="@/assets/logo.png" class="w-28" alt="logo cashcash">
         </div>
         
-      <div>
-        <ul class="flex flex-col text-xl justify-start">
-            
-              <router-link to="/">
-                <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/' }" class="mt-5 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
-                  Dashboard
-                </li>
-              </router-link>
-            
-              <router-link to="/client">
-                <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/client' }" class="mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
-                  <p class="pl-6"><i class="fa-solid fa-user-group"></i> Clients</p>
-                </li>
-              </router-link>
+        <div>
+          <ul class="flex flex-col text-xl justify-start">
+              
+                <router-link to="/">
+                  <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/' }" class="mt-5 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
+                    <p class="pl-6"><i class="fa-solid fa-grip"></i> Dashboard</p>
+                  </li>
+                </router-link>
+              
+                <router-link to="/client">
+                  <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/client' }" class="mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
+                    <p class="pl-6"><i class="fa-solid fa-user-group"></i> Clients</p>
+                  </li>
+                </router-link>
 
-              <router-link to="/statistique">
-                <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/statistique' }" class="mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
-                  <p class="pl-6"><i class="fa-solid fa-chart-simple"></i> Statistiques</p>
-                </li>
-              </router-link>
-            
-              <router-link to="/intervention">
-                <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/intervention' }" class="mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
-                  <p class="pl-6"><i class="fa-solid fa-truck"></i> Interventions</p>
-                </li>
-              </router-link>
+                <router-link to="/statistique">
+                  <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/statistique' }" class="mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
+                    <p class="pl-6"><i class="fa-solid fa-chart-simple"></i> Statistiques</p>
+                  </li>
+                </router-link>
+              
+                <router-link to="/intervention">
+                  <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/intervention' }" class="mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
+                    <p class="pl-6"><i class="fa-solid fa-truck"></i> Interventions</p>
+                  </li>
+                </router-link>
 
-              <router-link to="/technicien">
-                <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/technicien' }" class=" mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
-                  <p class="pl-6"><i class="fa-solid fa-headset"></i> Techniciens</p>
-                </li>
-              </router-link>
-        </ul>
-      </div>
-
+                <router-link to="/technicien">
+                  <li :class="{ 'text-white bg-[#3241B3]': $route.path === '/technicien' }" class=" mt-1 py-[7px] hover:text-white hover:bg-[#3241B3] w-full text-left rounded-lg">
+                    <p class="pl-6"><i class="fa-solid fa-headset"></i> Techniciens</p>
+                  </li>
+                </router-link>
+          </ul>
+        </div>
       </div>
   
       <!-- main -->
-      <main>
-
-
-        <router-view />
-
-
-      </main>
+        <main class="flex-1">
+          <router-view />
+        </main>
     </div>
 
 
