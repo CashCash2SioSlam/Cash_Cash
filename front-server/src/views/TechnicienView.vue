6bf29d193bad939d7b9c1334139f1e841e9b40fa
{Karasun} <{diego.torregano@gmail.com}>
2024-01-24 09:29:52 +0100

    modif front technicien.vue

--- front-server/src/views/TechnicienView.vue
+++ front-server/src/views/TechnicienView.vue
@@ -1,6 +1,43 @@
 <template>
-    <div class="about">
-      <h1 class="text-black">This is an technicien page</h1>
+  <div class="technicien">
+    
+<div class=" bg-white rounded-3xl h-full mr-12">
+
+
+<div class="my-10 text-center">
+<form>
+  <input class="bg-[#EDEDED] rounded-xl px-3 py-2 mt-10" type="text" id="" placeholder="Recherche">   <!-- *v-model="searchQuery -->
+  <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+</form>
+</div>
+
+
+
+    <div class="mx-[60px] bg-[#3241B3] p-3 rounded-2xl overflow-scroll max-h-screen">
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg x-2">
+        <p class="ml-5">Eddie Cruze - 26/04/1984 - 0678542300 - Technicien - Eddie.cruze@cash_cash.com <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg mt-2">
+        <p class="ml-5">Carle Thomson - 07/08/1992 - 0645952321 - Technicien - Carle.thomson@cash_cash.com <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg mt-2">
+        <p class="ml-5">James Brimo - 20/02/1989 - 0678542300 - Technicien - James.brimo@cash_cash.com <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg mt-2">
+        <p class="ml-5">Jack Torrent - 09/11/1995 - 0678002152 - Technicien - Jack.torrent@cash_cash.com <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
     </div>
-  </template>
-  
+
+
+</div>
+
+  </div>
+</template>
+
+<style>
+
+.technicien{
+height: 100%;
+}
+
+</style>
