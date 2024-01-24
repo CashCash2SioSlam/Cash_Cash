1f702797bdc5f6d806a385c91a522eaefee6ae36
{Karasun} <{diego.torregano@gmail.com}>
2024-01-24 08:33:50 +0100

    rebuild of the project

--- front-server/README.md
+++ front-server/README.md
@@ -23,14 +23,5 @@ npm run lint
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
+
 ### Start Backend
-
-npm install express mariadb
-
-npm i --save-dev @types/express
-
-npm install -g nodemon 
-
-npm install ts-node
-
-npm install --save-dev nodemon
---
+++ front-server/src/views/InterventionView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="client">
+    
+<div class=" bg-white rounded-3xl h-full mr-12">
+
+
+<div class="my-10 text-center">
+<form>
+  <input class="bg-[#EDEDED] rounded-xl px-3 py-2 mt-10" type="text" id="" placeholder="Recherche">   <!-- *v-model="searchQuery -->
+  <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+</form>
+</div>
+
+
+
+    <div class="mx-[60px] bg-[#3241B3]">
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg x-2">
+        <p class="ml-5">Leroy Merlin - 26/04/2024 - <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg mt-2">
+        <p class="ml-5">Carrefour - 27/04/2024 - <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg mt-2">
+        <p class="ml-5">Leroy Merlin - 28/04/2024 - <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
+      <div class="bg-[#EDEDED] py-[10px] rounded-lg mt-2">
+        <p class="ml-5">Carrefour - contact@carrefour.fr - 29/04/2024 -  <i class="fa-solid fa-download"></i> voir plus</p>
+      </div>
+    </div>
+
+
+</div>
+
+  </div>
+</template>
+
+<style>
+
+.client{
+height: 100%;
+}
+
+</style>
